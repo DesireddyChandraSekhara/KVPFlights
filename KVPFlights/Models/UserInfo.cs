6c1061e5dae05328bb8c10e66033e84d56c1073d
Chandrasekhara Reddy Desi Reddy Kasivaraprasada <chandrasekhara.desireddy@hallmarkhcs.com>
2022-08-03 22:38:37 +0530

    ignore unwanted properties

--- KVPFlights/Models/UserInfo.cs
+++ KVPFlights/Models/UserInfo.cs
@@ -1,13 +1,22 @@
-﻿namespace KVPFlights.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace KVPFlights.Models
 {
     public class UserInfo
     {
 
+        [JsonIgnore]
+       
         public int UserId { get; set; }
-        public string DisplayName { get; set; }
-        public string UserName { get; set; }
+        [JsonIgnore]
+        public string? DisplayName { get; set; }
+        [JsonIgnore]
+        public string? UserName { get; set; }
         public string? Email { get; set; }
         public string? Password { get; set; }
+
+        [JsonIgnore]
         public DateTime CreatedDate { get; set; }
     }
 }
